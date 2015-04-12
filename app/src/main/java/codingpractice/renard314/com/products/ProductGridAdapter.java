52589e1c040a2a05758b80e5c71ac652b7dddf89
Renard Wellnitz <renard.wellnitz@googlemail.com>
2015-04-12 11:43:01 +0200

    feat: fix palette extraction

--- app/src/main/java/codingpractice/renard314/com/products/ProductGridAdapter.java
+++ app/src/main/java/codingpractice/renard314/com/products/ProductGridAdapter.java
@@ -1,11 +1,14 @@
 package codingpractice.renard314.com.products;
 
 import android.annotation.SuppressLint;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.v4.util.Pair;
 import android.support.v7.graphics.Palette;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +17,25 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.nineoldandroids.animation.AnimatorSet;
 import com.nineoldandroids.animation.ArgbEvaluator;
 import com.nineoldandroids.animation.ValueAnimator;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Request;
+import com.squareup.picasso.RequestHandler;
+import com.squareup.picasso.Target;
 import com.squareup.picasso.Transformation;
 
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -38,27 +49,86 @@ public class ProductGridAdapter extends BaseAdapter {
 
 
     private static final String TAG = ProductGridAdapter.class.getSimpleName();
+    private static final Map<String, Pair<Integer, Integer>> mColorCache = new HashMap<>();
     final private List<Product> mProducts = new ArrayList<>();
     final private LayoutInflater mLayoutInflater;
+    private final int mDefaultBackgroundColor;
+    private final int mDefaultTextColor;
     private int mColumnWidth = 0;
     private int mImageHeight = 0;
 
 
     final static class ProductViewHolder {
-        @InjectView(R.id.text_container) View mTextContainer;
-        @InjectView(R.id.measure_text_view) TextView mMeasureView;
-        @InjectView(R.id.title_text_view) TextView titleView;
-        @InjectView(R.id.price_text_view) TextView priceView;
-        @InjectView(R.id.product_image_view) ImageView imageView;
+        @InjectView(R.id.text_container)
+        View mTextContainer;
+        @InjectView(R.id.measure_text_view)
+        TextView mMeasureView;
+        @InjectView(R.id.title_text_view)
+        TextView titleView;
+        @InjectView(R.id.price_text_view)
+        TextView priceView;
+        @InjectView(R.id.product_image_view)
+        ImageView imageView;
+        private AnimatorSet mAnimatorSet;
 
 
         public ProductViewHolder(View convertView) {
             ButterKnife.inject(this, convertView);
         }
+
+        public synchronized void stopColorAnimation() {
+            if (mAnimatorSet != null && (mAnimatorSet.isRunning() || mAnimatorSet.isStarted())) {
+                mAnimatorSet.cancel();
+            }
+        }
+
+        public void startColorAnimation(final Pair<Integer, Integer> colors) {
+            mTextContainer.post(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (this) {
+                        stopColorAnimation();
+                        mAnimatorSet = new AnimatorSet();
+
+                        int toColor = colors.first;
+                        ValueAnimator backGroundColorAnimator = new ValueAnimator();
+                        int fromColor = ((ColorDrawable) mTextContainer.getBackground()).getColor();
+                        backGroundColorAnimator.setIntValues(fromColor, toColor);
+                        backGroundColorAnimator.setEvaluator(new ArgbEvaluator());
+                        backGroundColorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                mTextContainer.setBackgroundColor((Integer) valueAnimator.getAnimatedValue());
+                            }
+                        });
+
+                        toColor = colors.second;
+                        ValueAnimator textColorAnimator = new ValueAnimator();
+                        fromColor = titleView.getCurrentTextColor();
+                        textColorAnimator.setIntValues(fromColor, toColor);
+                        textColorAnimator.setEvaluator(new ArgbEvaluator());
+                        textColorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                titleView.setTextColor((Integer) valueAnimator.getAnimatedValue());
+                            }
+                        });
+
+                        mAnimatorSet.setDuration(300).playTogether(backGroundColorAnimator, textColorAnimator);
+                        mAnimatorSet.start();
+                    }
+
+                }
+            });
+
+        }
     }
 
     public ProductGridAdapter(LayoutInflater layoutInflater, Collection<Product> products) {
         mProducts.addAll(products);
+        final Resources resources = layoutInflater.getContext().getResources();
+        mDefaultBackgroundColor = resources.getColor(R.color.colorAccent);
+        mDefaultTextColor = resources.getColor(R.color.primary_text_default_material_dark);
         this.mLayoutInflater = layoutInflater;
     }
 
@@ -95,7 +165,19 @@ public class ProductGridAdapter extends BaseAdapter {
             holder = (ProductViewHolder) convertView.getTag();
         }
         final Product product = mProducts.get(position);
-        if(mColumnWidth==0) {
+        startLoadImage(convertView, holder, product);
+        //US $ or Singapore $ looks the same to me but a proper shopping app handles currencies differently.
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        holder.priceView.setText(format.format(product.pricing.price));
+        holder.titleView.setText(product.title);
+        holder.mMeasureView.setText(product.measure.wt_or_vol);
+
+        return convertView;
+    }
+
+    private void startLoadImage(View convertView, final ProductViewHolder holder, final Product product) {
+        holder.stopColorAnimation();
+        if (mColumnWidth == 0) {
             final View finalConvertView = convertView;
             holder.imageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
@@ -107,7 +189,7 @@ public class ProductGridAdapter extends BaseAdapter {
                     mImageHeight = holder.imageView.getHeight();
 
 
-                    loadImage(finalConvertView,mColumnWidth,mImageHeight, holder, product);
+                    loadImage(finalConvertView, mColumnWidth, mImageHeight, holder, product);
 
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                         holder.imageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -117,88 +199,102 @@ public class ProductGridAdapter extends BaseAdapter {
                 }
             });
         } else {
-            loadImage(convertView, mColumnWidth,mImageHeight, holder, product);
+            loadImage(convertView, mColumnWidth, mImageHeight, holder, product);
         }
-        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
-        holder.priceView.setText(format.format(product.pricing.price));
-        holder.titleView.setText(product.title);
-        final int accentColor = holder.mMeasureView.getResources().getColor(R.color.colorAccent);
-        holder.mTextContainer.setBackgroundColor(accentColor);
-        holder.mMeasureView.setText(product.measure.wt_or_vol);
-
-
-        return convertView;
     }
 
-    private void loadImage(View convertView, int itemWidth, int itemHeight, ProductViewHolder holder, Product product) {
+    private void loadImage(View convertView, int itemWidth, int itemHeight, final ProductViewHolder holder, final Product product) {
 
+        //colors will be in cache most likely
+        Pair<Integer, Integer> colors;
+        Callback callBack;
+        synchronized (mColorCache){
+            colors = mColorCache.get(product.img.name);
+        }
+        if(colors != null){
+            holder.mTextContainer.setBackgroundColor(colors.first);
+            holder.titleView.setTextColor(colors.second);
+            callBack=null;
+        } else {
+            holder.mTextContainer.setBackgroundColor(mDefaultBackgroundColor);
+            holder.titleView.setTextColor(mDefaultTextColor);
+            callBack = new Callback() {
+                @Override
+                public void onSuccess() {
+                    Pair<Integer, Integer> colors = null;
+                    //colors will be in cache most likely
+                    synchronized (mColorCache){
+                        colors = mColorCache.get(product.img.name);
+                    }
+                    if(colors == null){
+                        colors = Pair.create(mDefaultBackgroundColor, mDefaultTextColor);
+                    }
+                    holder.startColorAnimation(colors);
+
+                }
+
+                @Override
+                public void onError() {
+                    final Pair<Integer, Integer> colors = Pair.create(mDefaultBackgroundColor, mDefaultTextColor);
+                    holder.startColorAnimation(colors);
+                }
+            };
+        }
+
+
+
+        final PaletteExtractor paletteExtractor = new PaletteExtractor(product.img.name, mColorCache);
         Picasso.with(convertView.getContext().getApplicationContext())
                 .load("http://media.redmart.com/newmedia/200p" + product.img.name)
                 .resize(itemWidth, itemHeight)
                 .centerInside()
-                .transform(new PaletteExtractor(holder))
-                .into(holder.imageView);
+                .transform(paletteExtractor)
+                .into(holder.imageView, callBack);
     }
 
+    /**
+     * extracts colors from a bitmap while loading it. the result is placed in mColorCache.
+     */
     private static class PaletteExtractor implements Transformation {
+        private final Map<String, Pair<Integer, Integer>> mColorCache;
 
-        private final ProductViewHolder mHolder;
 
-        PaletteExtractor(ProductViewHolder holder){
+        private final String mBitmapName;
 
-            mHolder = holder;
+        PaletteExtractor(String bitmapName, Map<String, Pair<Integer, Integer>> colorCache) {
+            mBitmapName = bitmapName;
+            mColorCache =colorCache;
         }
 
+
+
         @Override
-        public Bitmap transform(final Bitmap source) {
-            final Palette palette = Palette.generate(source);
-            mHolder.mTextContainer.post(new Runnable() {
-                @Override
-                public void run() {
-                    if(palette.getMutedSwatch()==null){
-                        return;
+        public Bitmap transform(Bitmap source) {
+            Pair<Integer, Integer> colors;
+            synchronized (mColorCache) {
+                Log.i(TAG, "extract color from " + mBitmapName + " cache hit.");
+                colors = mColorCache.get(mBitmapName);
+            }
+            if (colors == null) {
+                final Palette palette = Palette.generate(source);
+                if (palette.getMutedSwatch() != null) {
+                    colors = Pair.create(palette.getMutedSwatch().getRgb(), palette.getMutedSwatch().getTitleTextColor());
+                    synchronized (mColorCache) {
+                        mColorCache.put(mBitmapName, colors);
                     }
-                    final int color = palette.getMutedSwatch().getRgb();
-                    ValueAnimator anim = new ValueAnimator();
-                    final int color1 = ((ColorDrawable) mHolder.mTextContainer.getBackground()).getColor();
-                    anim.setIntValues(color1, color);
-                    anim.setEvaluator(new ArgbEvaluator());
-                    anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                        @Override
-                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                            mHolder.mTextContainer.setBackgroundColor((Integer) valueAnimator.getAnimatedValue());
-                        }
-                    });
-
-                    anim.setDuration(200);
-                    anim.start();
-                    final int textColor = palette.getMutedSwatch().getTitleTextColor();
-                    ValueAnimator anim2 = new ValueAnimator();
-                    final int textColor1 = mHolder.titleView.getCurrentTextColor();
-                    anim2.setIntValues(textColor1, textColor);
-                    anim2.setEvaluator(new ArgbEvaluator());
-                    anim2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                        @Override
-                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                            mHolder.titleView.setTextColor((Integer) valueAnimator.getAnimatedValue());
-                        }
-                    });
-
-                    anim2.setDuration(200);
-                    anim2.start();
-
+                    Log.i(TAG, "extract color from " + mBitmapName + " rgb = " + String.format("%06X", colors.first & 0xFFFFFF));
+                } else {
+                    Log.i(TAG, "extract color from " + mBitmapName + " failed.");
                 }
-            });
 
-            //mHolder.mTextContainer.setBackgroundColor(color);
-            //mHolder.titleView.setTextColor(textColor);
-
+            }
             return source;
+
         }
 
         @Override
         public String key() {
-            return "PaletteExtractor";
+            return PaletteExtractor.class.getSimpleName();
         }
     }
 }
